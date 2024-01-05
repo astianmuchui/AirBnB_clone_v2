3534e738d41c21debb0655eeb347f946c4005022
astianmuchui <astianmuchui@gmail.com>
2024-01-05 10:12:17 +0300

    python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Starts a flask server and returns
+Hello HBNB in the root route
+HBNB in the hbnb route
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    The root route
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """
+    The hbnb route
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+  text = text.replace("_", " ")
+  return "C {}".format(text)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+  text = text.replace("_", " ")
+  return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000)
+
