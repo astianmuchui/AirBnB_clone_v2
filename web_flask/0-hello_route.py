bedf00a14df3d2e02d1a931e5d81bd788b986bd2
astianmuchui <astianmuchui@gmail.com>
2024-01-05 09:46:42 +0300

    Added module documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,17 @@
 Starts a flask server and returns
 Hello HBNB in the root route
 """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
-  return "Hello HBNB!"
+    """
+        The root route
+    """
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
-  app.run()
+  app.run(host="0.0.0.0", port=5000)
