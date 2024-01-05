0d77fbfe69b66f746b66ddfe4b0fe54d19940d11
astianmuchui <astianmuchui@gmail.com>
2024-01-05 11:06:26 +0300

    number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Starts a flask server and returns
+Hello HBNB in the root route
+HBNB in the hbnb route
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    The root route
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+  """The hbnb route"""
+
+  return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+  """
+  The C route
+  """
+  
+  text = text.replace("_", " ")
+  return "C {}".format(text)
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text):
+  """
+  The python route
+  """  
+
+  text = text.replace("_", " ")
+  return "Python {}".format(text)
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+  """
+  The number route
+  """
+  
+  return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+  """
+  The number template route
+  """
+  return render_template("5-number.html", number=n)
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000)
+
